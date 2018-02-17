792f145134b1c3eda9cc127ec96beae6bb9baf41
Samuel Cha <sunchipssam@gmail.com>
2018-02-17 08:37:54 -0800

    Added initial styling with less

--- foodggregator/src/components/app/App.js
+++ foodggregator/src/components/app/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from '../header/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ foodggregator/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = _ => {
+  return (
+    <div>
+      <div className="Header">Foodggregator</div>
+    </div>
+  );
+};
+
+export default Header;
