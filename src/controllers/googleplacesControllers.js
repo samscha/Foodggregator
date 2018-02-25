929a8010ef3bab0ba3103db6d7ce6c5c427d4a33
Samuel Cha <sunchipssam@gmail.com>
2018-02-25 03:01:19 -0800

    Added db checking of places

--- src/controllers/googleplacesControllers.js
+++ src/controllers/googleplacesControllers.js
@@ -17,24 +17,101 @@ const getGooglePlacesDetailsUrl = placeId => {
 const createGoogleplacePlacesFrom = data => {
   return data.results.map(place => {
     return new Promise((resolve, reject) => {
-      fetchAPI(getGooglePlacesDetailsUrl(place.place_id)).then(details =>
-        Googleplace({
-          name: place.name,
-          address: place.formatted_address,
-          intlPhone: details.result.international_phone_number
-            ? details.result.international_phone_number.replace(/[ -]/g, '')
-            : '',
-          location: { lat: place.geometry.lat, lng: place.geometry.lng },
-          rating: place.rating,
-          id: place.id,
-          placeId: place.place_id,
+      Googleplace.find({ id: place.id })
+        .then(results => {
+          results.length > 0
+            ? resolve(results)
+            : resolve(
+                new Promise((resolve, reject) => {
+                  fetchAPI(getGooglePlacesDetailsUrl(place.place_id))
+                    .then(details =>
+                      Googleplace({
+                        name: place.name,
+                        address: place.formatted_address,
+                        intlPhone: details.result.international_phone_number
+                          ? details.result.international_phone_number.replace(
+                              /[ -]/g,
+                              '',
+                            )
+                          : '',
+                        location: {
+                          lat: place.geometry.lat,
+                          lng: place.geometry.lng,
+                        },
+                        rating: place.rating,
+                        id: place.id,
+                        placeId: place.place_id,
+                      })
+                        .save()
+                        .then(savedPlace => resolve(savedPlace))
+                        .catch(err => reject(err)),
+                    )
+                    .catch(err => reject(err));
+                }),
+              );
         })
-          .save()
-          .then(savedPlace => resolve(savedPlace))
-          .catch(err => reject(err)),
-      );
+        .catch(err => reject(err));
     });
   });
 };
+
+// return Googleplace.find({ id: place.id })
+//   .then(results => {
+//     results.length > 0
+//       ? Promise.resolve(results)
+//       : new Promise((resolve, reject) => {
+//           fetchAPI(getGooglePlacesDetailsUrl(place.place_id))
+//             .then(details =>
+//               Googleplace({
+//                 name: place.name,
+//                 address: place.formatted_address,
+//                 intlPhone: details.result.international_phone_number
+//                   ? details.result.international_phone_number.replace(
+//                       /[ -]/g,
+//                       '',
+//                     )
+//                   : '',
+//                 location: {
+//                   lat: place.geometry.lat,
+//                   lng: place.geometry.lng,
+//                 },
+//                 rating: place.rating,
+//                 id: place.id,
+//                 placeId: place.place_id,
+//               })
+//                 .save()
+//                 .then(savedPlace => resolve(savedPlace))
+//                 .catch(err => reject(err)),
+//             )
+//             .catch(err => reject(err));
+//         });
+//   })
+//   .catch(err => console.log(err));
+
+// if (dbSearch) {
+//   console.log(dbSearch);
+//   return Promise.resolve(dbSearch[0]);
+// }
+
+// return new Promise((resolve, reject) => {
+//   fetchAPI(getGooglePlacesDetailsUrl(place.place_id)).then(details =>
+//     Googleplace({
+//       name: place.name,
+//       address: place.formatted_address,
+//       intlPhone: details.result.international_phone_number
+//         ? details.result.international_phone_number.replace(/[ -]/g, '')
+//         : '',
+//       location: { lat: place.geometry.lat, lng: place.geometry.lng },
+//       rating: place.rating,
+//       id: place.id,
+//       placeId: place.place_id,
+//     })
+//       .save()
+//       .then(savedPlace => resolve(savedPlace))
+//       .catch(err => reject(err)),
+//   );
+// });
+//   });
+// };
 
 module.exports = { getGoogleplacesTextsearchUrl, createGoogleplacePlacesFrom };
