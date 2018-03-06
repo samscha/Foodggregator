e5f747cfde457f3eb21f59f3aabe4dc4d2cbe300
Samuel Cha <sunchipssam@gmail.com>
2018-03-05 16:24:12 -0800

    Deleted comments in places route

--- src/routes/places.js
+++ src/routes/places.js
@@ -43,48 +43,6 @@ router.get(
           .catch(err => res.status(SERVER_ERROR).json(err));
       })
       .catch(reason => res.status(SERVER_ERROR).json(reason));
-
-    //   const geocode = req.geocode;
-    //   const location = req.geocodeLoc;
-    //   const { query } = req.query;
-    //   const googleplacesUrl = getGoogleplacesTextsearchUrl(query, location);
-    //   const { yelpUrl, yelpOptions } = getYelpBusinessesSearchUrl(
-    //     query,
-    //     location,
-    //   );
-    //   Promise.all([
-    //     cache[googleplacesUrl]
-    //       ? Promise.resolve(cache[googleplacesUrl])
-    //       : fetchAPI(googleplacesUrl),
-    //     cache[yelpUrl]
-    //       ? Promise.resolve(cache[yelpUrl])
-    //       : fetchAPIWith(yelpUrl, yelpOptions),
-    //   ])
-    //     .then(values => {
-    //       Promise.all([
-    //         Promise.all(createGoogleplacePlacesFrom(values[0])),
-    //         Promise.all(createYelpPlacesFrom(values[1])),
-    //       ])
-    //         .then(values => {
-    //           const combinedPlaces = comebineThese(values);
-    //           Promise.all(createPlacesFrom(combinedPlaces))
-    //             .then(values => {
-    //               // findPlacesWith(values[0].concat(values[1]))
-    //               // console.log(values);
-    //               // value =>
-    //               findPlacesWith(values)
-    //                 .then(places => res.status(OK).json([geocode].concat(places)))
-    //                 .catch(err => res.status(SERVER_ERROR).json(err));
-    //             })
-    //             .catch(err => res.status(SERVER_ERROR).json(err));
-    //           // console.log('results for route', values);
-    //           // res.status(OK).json(findPlacesWith(values[0].concat(values[1])));
-    //         })
-    //         .catch(err => res.status(SERVER_ERROR).json(err));
-    //     })
-    //     .catch(err => res.status(SERVER_ERROR).json(err));
-    //   // })
-    //   // .catch(err => res.status(SERVER_ERROR).json(err));
   },
 );
 
